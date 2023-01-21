e0d0d422cb09b9ef574abe4f24080b56afe01d09
Dashrath-Sharma <dashrath9958@gmail.com>
2023-01-21 17:49:20 +0530

    Home & Search Styles

---
+++ src/components/search/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css'
+
+const Search = () => {
+    return (
+        <form className="Search">
+            <input name="search" placeholder="Search Recipes" id="search" />
+            <button type="submit">Search</button> 
+        </form>
+    )
+}
+
+export default Search
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+import Search from "../../components/search"
+
+const Home = () => {
+    return (
+        <div className="Home">
+            <Search />
+        </div>
+    )
+}
+
+export default Home
